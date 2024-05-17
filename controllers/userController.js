b00fb7267549876a3429d558e6dd5d6f980b3104
Carlos Salinas <73559919+csalinas14@users.noreply.github.com>
2024-05-17 19:30:40 -0400

    Update userController.js
    adding error checks from req1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,28 @@
-//const { sendServerError, sendOkResponse } = require("../../core/responses")
 const bcrypt = require("bcrypt")
 const User = require("../models/user")
 
 const createUser = async (req, res) => {
   try{
-
+    
+    if(!req.body.password){
+      return res.status(400).json({error: "Please provide a password!"})
+    }
+    
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(req.body.password, saltRounds)
     const user = await User.create({...req.body, password: passwordHash})
     
     res.json(user)
   } catch (error){
-    
+
+    if(error.name === 'SequelizeValidationError'){
+      let errorMsgAry = []
+      for(let i=0; i < error.errors.length; i++){
+        errorMsgAry.push(error.errors[i].message)
+      }
+      const errorMsg = errorMsgAry.join(', ')
+      return res.status(500).json({error: errorMsg})
+    }
     res.status(400).json({error: error.message})
   }
 }

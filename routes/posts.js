a8084bccf1039ef09a1614111dd7ab7e78b538de
Carlos Salinas <73559919+csalinas14@users.noreply.github.com>
2024-05-17 15:14:24 -0400

    Update posts.js
    adding userExtractor to change post

--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,7 @@ router.post("/", userExtractor, createPost)
 
 router.get("/:id", getPost)
 
-router.put("/:id", changePost)
+router.put("/:id", userExtractor, changePost)
 
 router.get("/", getPaginationPosts)
 
